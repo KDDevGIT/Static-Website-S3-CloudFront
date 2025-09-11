d159c4f759f5b67f1f72751540e58b6e9806ebed
KDDevGIT <kymasterbox@gmail.com>
2025-09-11 16:55:12 -0700

    Added cloudfront_domain_name to outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,8 @@ output "bucket_name" {
   description = "S3 Bucket Name for Site"
 }
 
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.this.domain_name
+  description = "CloudFront Domain to Access Site"
+}
+
