6cd2768675a685922c6b5363ba14d2629da67b55
KDDevGIT <kymasterbox@gmail.com>
2025-09-11 07:26:18 -0700

    Added site variables for s3 bucket to s3.tf

---
+++ terraform/s3.tf
@@ -0,0 +1,23 @@
+# For S3 Bucket
+resource "aws_s3_bucket" "site" {
+  bucket = local.bucket_name
+  force_destroy = true
+  tags = {
+    Project = var.project_name
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "site" {
+  bucket = aws_s3_bucket.site.id 
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id 
+  block_public_acls = true
+  block_public_policy = true 
+  ignore_public_acls = true 
+  restrict_public_buckets = true 
+}
