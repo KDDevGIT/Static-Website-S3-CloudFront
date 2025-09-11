4d235a3a334b9d9a1c23e4f9ee73cb35b29029d0
KDDevGIT <kymasterbox@gmail.com>
2025-09-11 15:37:34 -0700

    Added S3 Bucket Policy via JSON for created CloudFront distribution to cloudfront.tf

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -13,5 +13,71 @@ resource "aws_cloudfront_distribution" "this" {
   comment = "${var.project_name} static site"
   price_class = var.price_class 
   aliases = var.aliases
+
+  origins {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name 
+    origin_id = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id 
+  }
+  default_root_object = var.website_index
+  default_cache_behavior {
+    target_origin_id = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET","HEAD","OPTIONS"]
+    cached_methods = ["GET","HEAD"]
+    compress = true 
+    forwarded_values {
+      query_string = false 
+      cookies {
+        forward = "none"
+      }
+    }
+    min_ttl = 0
+    default_ttl = var.default_ttl
+    max_ttl = 86400
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true 
+  }
+  custom_error_response {
+    error_code = 404
+    response_code = 200
+    response_page_path = "/${var.website_error}"
+    error_caching_min_ttl = 0
+  }
+  tags = {
+    Project = var.project_name
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_cf" {
+  bucket = aws_s3_bucket.site.id 
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+         {
+        Sid      = "AllowCloudFrontServicePrincipalReadOnly",
+        Effect   = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action   = ["s3:GetObject"],
+        Resource = [
+          "${aws_s3_bucket.site.arn}/*"
+        ],
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.this.arn
+          }
+        }
+      }
+    ]
+  })
+  depends_on = [ aws_cloudfront_distribution.this ]
 }
 
